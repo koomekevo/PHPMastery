933cce47b801a95581001fb5a073d25170d1859e
koomekevo <koome.kevo@gurucrafts.net>
2022-12-22 15:28:50 +0300

    Add <br /> and simple arithmetic

--- helloworld.php
+++ helloworld.php
@@ -7,6 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-    <?php echo "Hello World"; ?>
+    <?php echo "Hello World"; ?><br />
+    <?php echo "Hello"." World"; ?><br />
+    <?php echo 2 + 3; ?>
 </body>
 </html>
