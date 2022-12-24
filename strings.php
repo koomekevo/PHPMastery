d6dad813b3b4d86e1b63bfc0cb94c13f831adf3a
koomekevo <koome.kevo@gurucrafts.net>
2022-12-24 15:37:06 +0300

    Add strings.php

--- strings.php
+++ strings.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
+    <title>String</title>
 </head>
+
 <body>
     <?php
-    // Single line comments
-    /* Double line comments
-       are written like this
-    */
+    echo "Hello World<br />";
+    $greeting = "Hello";
+    $target = "World";
+    $phrase = $greeting . " " . $target;
+    echo $phrase;
     ?>
-    <?php echo "Hello World"; ?><br />
-    <?php echo "Hello"." World"; ?><br />
-    <?php echo 2 + 3; ?>
 </body>
+
 </html>
